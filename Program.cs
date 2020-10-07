9e5f09572fba9bfe06d63865acefd8556531dd61
Calle Persson <Calle_1997@hotmail.se>
2020-10-07 14:38:14 +0200

    Change message to "Hola el mundo!"

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Sunfleet_gittest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola el mundo!");
         }
     }
 }
