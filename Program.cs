f5b5cfa33b63d548efdbbde773e735bfb27f9908
Calle Persson <Calle_1997@hotmail.se>
2020-10-07 14:28:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sunfleet_gittest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
